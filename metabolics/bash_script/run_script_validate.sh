d92d10e57e013553f7aa809fb56c7dfc6008f467
Tomislav <tomislav@proportunity.co>
2018-02-06 22:02:34 +0000

    automated the validation predictiopn

--- metabolics/bash_script/run_script_validate.sh
+++ metabolics/bash_script/run_script_validate.sh
@@ -15,20 +15,26 @@
 
 echo $SGE_TASK_ID
 
-training_input="/users/spjtcoi/brc_scratch/project_tomi/conrad/reanalyse/drug_naive/new_protocol_25thOct/IMPUTATION_SETS/input_files_BMI_2catg/dataset_${SGE_TASK_ID}.csv"
-test_input="/users/spjtcoi/brc_scratch/project_tomi/conrad/reanalyse/drug_naive/new_protocol_25thOct/PREDICTION/validation_input/bmi/dataset_${SGE_TASK_ID}.csv"
-output_dir="/users/spjtcoi/brc_scratch/project_tomi/conrad/reanalyse/drug_naive/new_protocol_25thOct/PREDICTION/output/BMI/"
+input_dir=ASDAWDA
+training_input=${input_dir}/training_set_${SGE_TASK_ID}.csv
+test_input=${input_dir}/validation_set_${SGE_TASK_ID}.csv
+training_GE=${input_dir}/training_set_gene_expression.csv
+test_GE=${input_dir}/validation_set_gene_expression.csv
+feature_list=${input_dir}/validation_features.csv
+remove_feature_list=${input_dir}/demo.txt
+output_dir=my_output_dir
 
-
-# input parameters via commandline (trouble-shooting mode only)
-#output_dir=$1 #comment this out if running as batch
-#removing_features=$2 # comment this out if running as batch
-
-
-mkdir -p $output_dir #-p create multiple subdirectories on the fly (ie they dont already exist)
+echo "input_dir = $input_dir"
+echo "training_input  = $training_input"
+echo "test_input = $test_input"
+echo "training_GE = $training_GE"
+echo "test_GE = $test_GE"
+echo "feature_list = $feature_list"
+echo "remove_feature_list = $remove_feature_list"
+echo "output_dir = $output_dir"
 
 module load bioinformatics/R/3.4.1
-Rscript /users/spjtcoi/git/stat_learning_code/classification_prediction.R $training_input $test_input $output_dir> $output_dir/test.${SGE_TASK_ID}.out 2> $output_dir/test.${SGE_TASK_ID}.err
+Rscript /users/spjtcoi/git/stat_learning_code/classification_prediction.R $training_input $test_input $training_GE $test_GE $feature_list $remove_feature_list  $output_dir> $output_dir/test.${SGE_TASK_ID}.out 2> $output_dir/test.${SGE_TASK_ID}.err
 
 
 #Commandline demo
