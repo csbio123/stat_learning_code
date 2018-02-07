93a752d172e9c69bc1e7b1ed0f0161f806a3af56
Tomislav <tomislav@proportunity.co>
2018-02-07 21:21:39 +0000

    modifications to validation script. generalization of it

--- metabolics/predictions/validation_classification_prediction_tomi.R
+++ metabolics/predictions/validation_classification_prediction_tomi.R
@@ -1,5 +1,6 @@
 
 library(glmnet)
+library(caret)
 
 args <- commandArgs(trailingOnly = TRUE)#trailing only stops the argument function from requiring specification of too much information eg R version, etc
 
@@ -9,27 +10,55 @@ args <- commandArgs(trailingOnly = TRUE)#trailing only stops the argument functi
 #args[2] = "/Users/ti1/Google\ Drive/raw\ data/validation_data_classification/dataset_1.csv"
 #args[3] = "/Users/ti1/Google\ Drive/raw\ data/output/"
 
-args[1] = "/Users/ti1/Google\ Drive/raw data/batch_normalized_data/training_set_1.csv"
-args[2] = "/Users/ti1/Google\ Drive/raw data/batch_normalized_data/validation_set_1.csv"
-args[3] = "/Users/ti1/Google\ Drive/raw\ data/output/"
+#args[1] = "/Users/ti1/Google\ Drive/raw data/batch_normalized_data/training_set_1.csv"
+#args[2] = "/Users/ti1/Google\ Drive/raw data/batch_normalized_data/validation_set_1.csv"
+#args[3] = "/Users/ti1/Google\ Drive/raw data/batch_normalized_data/training_set_gene_expression.csv"
+#args[4] = "/Users/ti1/Google\ Drive/raw data/batch_normalized_data/validation_set_gene_expression.csv"
+#args[5] = "~/Downloads/test.csv"
+#args[6] = "/Users/ti1/Google\ Drive/raw\ data/remove_demo.txt"
+#args[7] = "/Users/ti1/Google\ Drive/raw\ data/test_output-dir"
 
 print(paste0('GLMNET analaysis'))
 print(paste0('Dataset train:', args[1]))
 print(paste0('Dataset test: ', args[2] ))
-print(paste0('Output dir: ', args[3] ))
+print(paste0('Genes train: ', args[3] ))
+print(paste0('Genes test: ', args[4] ))
+print(paste0('All features: ', args[5] ))
+print(paste0('To remove features: ', args[6] ))
+print(paste0('Output-dir: ', args[7] ))
+
+number = Sys.getenv(c("SGE_TASK_ID"))
+
 
 data_train = read.csv(args[1], stringsAsFactors = FALSE)
 data_test = read.csv(args[2], stringsAsFactors = FALSE)
+gene_expression_train = read.csv(args[3], stringsAsFactors = FALSE)
+gene_expression_test = read.csv(args[4], stringsAsFactors = FALSE)
 
-gene_expression_train = read.csv("/Users/ti1/Google\ Drive/raw data/batch_normalized_data/training_set_gene_expression.csv", stringsAsFactors = FALSE)
-gene_expression_test = read.csv("/Users/ti1/Google\ Drive/raw data/batch_normalized_data/validation_set_gene_expression.csv", stringsAsFactors = FALSE)
+#This is the fike which contains all features and its corresponding class
+#Column 1: Feature name
+#Column 2: Feature class
+if(!file.exists(args[5])) {
+  stop("Feature reference file not found. Please check.")
+} else {
+  feature_list = read.csv(args[5], stringsAsFactors = FALSE)
+}
 
+#This file lists all classes and/or features that should be removed. It is a 1-dim list.
+#If this file doesn't exist, nothing will be removed
+if(file.exists(args[6])) {
+  print(paste0('Features to remove: ', args[6] ))
+  features_to_remove = unlist(read.csv(args[6], stringsAsFactors = FALSE, header=FALSE))
+} else {
+  features_to_remove = c()
+}
 
 data_train = cbind(data_train, gene_expression_train)
 data_test = cbind(data_test, gene_expression_test)
 
 same_columns = intersect(colnames(data_train), colnames(data_test))
 
+#write.csv(same_columns, "~/Downloads/test.csv")
 data_train = data_train[, which(colnames(data_train) %in% same_columns)]
 data_test = data_test[, which(colnames(data_test) %in% same_columns)]
 
@@ -42,8 +71,26 @@ Y_test = factor(data_test[, which(colnames(data_test) %in% target_columns)])
 X_train = data_train[, -which(colnames(data_train) %in% target_columns)]
 X_test = data_test[, -which(colnames(data_test) %in% target_columns)]
 
-X_train=X_train[,1:7]
-X_test=X_test[,1:7]
+#Remove features from the class
+if(length(features_to_remove) >0) {
+  
+  features_to_remove=unlist(features_to_remove)
+  print("Following features will be removed:")
+  
+  top_level_to_remove = feature_list[feature_list[,2] %in% features_to_remove, ]
+  specific_to_remove = feature_list[feature_list[,1] %in% features_to_remove,]
+  
+  print("Classess:")
+  print(unique(top_level_to_remove[,2]))
+  
+  print("Specific features:")
+  print(unique(specific_to_remove[,1]))
+  
+  remove = unique(c(top_level_to_remove[,1], specific_to_remove[,1]))
+
+  X_train = X_train[,-which(colnames(X_train) %in% remove)]
+  X_test = X_test[,-which(colnames(X_test) %in% remove)]
+}
 
 previous_na_action <- options('na.action')
 options(na.action='na.pass')
@@ -69,7 +116,7 @@ X_test = X_test[, which(colnames(X_test) %in% same_columns)]
 print("Training GLMNet model")#
 
 
-alphas <- c(seq(from = 0, to = 0.01, by = 0.0001), seq(from = 0.1, to = 1, by = 0.1))
+alphas <- c(seq(from = 0, to = 0.1, by = 0.0001), seq(from = 0.1, to = 1, by = 0.1))
 
 alpha_errors = unlist(foreach(i = 1:length(alphas), .inorder = FALSE,.packages = "glmnet", .multicombine = TRUE,.export= c("target","alphas","features")) %dopar% {
   #Run GLMNET witt our features and our target variable
@@ -84,7 +131,6 @@ alpha_errors = unlist(foreach(i = 1:length(alphas), .inorder = FALSE,.packages =
 })
 
 best_alpha = alphas[which.max(alpha_errors)]
-print(best_alpha)
 glmnet.fit <- cv.glmnet(x=X_train, y=Y_train, family='binomial', alpha = best_alpha ) #0.009
 
 # Generate predictions
@@ -93,20 +139,9 @@ preds <- as.numeric(unlist(predict(glmnet.fit, newx = X_test, type='class', s='l
 
 # Put results into dataframe for plotting.
 print("Compiling results table")
-results <- data.frame(prediction=preds, actual=Y_test)
-colnames(results) = c("pred", "actual")
-
-accuracy = Accuracy(results[["pred"]], results[["actual"]])
-recall =  Recall(results[["pred"]], results[["actual"]])
-precision =  Precision(results[["pred"]], results[["actual"]])
-print(accuracy)
-print(recall)
-print(precision)
-
-output_dir=arsgs[3]
-dir.create(output_dir, showWarnings = TRUE, recursive = TRUE)
-
-number = Sys.getenv(c("SGE_TASK_ID"))
+results <- data.frame(run_id=number,prediction=as.factor(preds), actual=as.factor(Y_test), alpha=best_alpha,)
 
 print("Writing file") #
-write.table(results, , file=paste0(output_dir, "/predictions_", number))
+output_dir=args[7]
+dir.create(output_dir, showWarnings = TRUE, recursive = TRUE)
+write.table(results, file=paste0(output_dir, "/predictions_", number))
