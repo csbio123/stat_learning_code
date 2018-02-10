531c50487b0b64aaebbf9265ebc87d8324d0f300
csbio <csbio@gmail.com>
2018-02-10 14:58:58 +0000

    all file update 10-02-18

--- metabolics/bash_script/run_script_validate.sh
+++ metabolics/bash_script/run_script_validate.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#$ -N validation1
+#$ -N medical_only
 #$ -q LowMemShortterm.q
-#$ -t 1
-#$ -pe smp 1
-#$ -l h_rt=00:02:00
+#$ -t 1-500
+#$ -pe smp 10
+#$ -l h_rt=00:10:00
 #$ -o /dev/null
 #$ -e /dev/null
 
@@ -21,8 +21,8 @@ test_input=${input_dir}/validation_set_${SGE_TASK_ID}.csv
 training_GE=${input_dir}/training_set_gene_expression.csv
 test_GE=${input_dir}/validation_set_gene_expression.csv
 feature_list=${input_dir}/../validation_features.csv
-remove_feature_list=${input_dir}/xyz.txt #If nothing to remove then add random (null) filename which will be ignored
-output_dir=/users/spjtcoi/git/stat_learning_code/metabolics/validation_prediction_new/batch_normalized_data/output_genes_and_clinical/
+remove_feature_list=${input_dir}/../medical_only.txt #If nothing to remove then add random (null) filename which will be ignored
+output_dir=/users/spjtcoi/git/stat_learning_code/metabolics/validation_prediction_new/batch_normalized_data/output_medical_only2/
 
 echo "input_dir = $input_dir"
 echo "training_input  = $training_input"
@@ -33,11 +33,11 @@ echo "feature_list = $feature_list"
 echo "remove_feature_list = $remove_feature_list"
 echo "output_dir = $output_dir"
 
-#module load bioinformatics/R/3.4.1
-export SGE_TASK_ID=50
+module load bioinformatics/R/3.4.1
+#export SGE_TASK_ID=50
 
 mkdir -p $output_dir
-Rscript /users/spjtcoi/git/stat_learning_code/metabolics/predictions/generate_training_validation_set_tomi.R $training_input $test_input $training_GE $test_GE $feature_list $remove_feature_list  $output_dir> $output_dir/test.${SGE_TASK_ID}.out 2> $output_dir/test.${SGE_TASK_ID}.err
+Rscript /users/spjtcoi/git/stat_learning_code/metabolics/predictions/validation_classification_prediction_tomi.R $training_input $test_input $training_GE $test_GE $feature_list $remove_feature_list  $output_dir> $output_dir/test.${SGE_TASK_ID}.out 2> $output_dir/test.${SGE_TASK_ID}.err
 
 
 #Commandline demo
