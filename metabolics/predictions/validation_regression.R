fc892e5344d4f474bee2effe4209464a5c7babc0
Tomislav <tomislav@proportunity.co>
2018-04-14 17:04:26 +0100

    addedd permutation testing on regression

--- metabolics/predictions/validation_regression.R
+++ metabolics/predictions/validation_regression.R
@@ -6,17 +6,19 @@ library(randomForest)
 before <- Sys.time() 
 
 args <- commandArgs(trailingOnly = TRUE)#trailing only stops the argument function from requiring specification of too much information eg R version, etc
-
 #This is just for testing purposes. If you don't make it as comment, it will overwrite any values that you have given over the command line 
+folder = "/users/spjtcoi/brc_scratch/project_tomi/conrad/reanalyse/drug_naive/new_protocol_25thOct/downloaded/"
+folder="/Users/ti1/Downloads/config/validation_prediction"
+args[1] = paste0(folder, "/train_sva_9_03_18/training_set_13.csv")
+args[2] = paste0(folder, "/validation_sva_9_03_18/validation_set_13.csv")
+args[3] = paste0(folder, "/train_sva_9_03_18/training_set_gene_expression.csv")
+args[4] = paste0(folder, "/validation_sva_9_03_18/validation_set_gene_expression.csv")
+args[5] = paste0(folder, "/feature_list_10_03_18.csv")
+args[6] = paste0(folder, "/remove_class.txt")
+args[7] = paste0(folder, "/test_output-dir")
+args[8] = 10
 
-args[1] = "/users/spjtcoi/brc_scratch/wgcna/Input/SVA/train_sva_9_03_18/training_set_1.csv"
-args[2] = "/users/spjtcoi/brc_scratch/wgcna/Input/SVA/validation_sva_9_03_18/validation_set_1.csv"
-args[3] = "/users/spjtcoi/brc_scratch/wgcna/Input/SVA/train_sva_9_03_18/training_set_gene_expression.csv"
-args[4] = "/users/spjtcoi/brc_scratch/wgcna/Input/SVA/validation_sva_9_03_18/validation_set_gene_expression.csv"
-args[5] = "/users/spjtcoi/brc_scratch/wgcna/Input/FEATURES/feature_list_10_03_18.csv"
-args[6] = "/users/spjtcoi/brc_scratch/wgcna/Input/Null.txt"
-args[7] = "/users/spjtcoi/brc_scratch/wgcna/Output/test_output-dir_28-03-18"
-
+iterations = args[8]
 
 print(paste0('GLMNET analaysis'))
 print(paste0('Dataset train:', args[1]))
@@ -26,6 +28,7 @@ print(paste0('Genes test: ', args[4] ))
 print(paste0('All features: ', args[5] ))
 print(paste0('To remove features: ', args[6] ))
 print(paste0('Output-dir: ', args[7] ))
+print(paste0('Permutations: ', iterations ))
 
 number = Sys.getenv(c("SGE_TASK_ID"))
 
@@ -34,6 +37,7 @@ data_train = read.csv(args[1], stringsAsFactors = FALSE)
 data_test = read.csv(args[2], stringsAsFactors = FALSE)
 gene_expression_train = read.csv(args[3], stringsAsFactors = FALSE)
 gene_expression_test = read.csv(args[4], stringsAsFactors = FALSE)
+
 #This is the fike which contains all features and its corresponding class
 #Column 1: Feature name
 #Column 2: Feature class
@@ -60,9 +64,7 @@ data_test = cbind(data_test, gene_expression_test)
 data_train = data_train[, which(colnames(data_train) %in% feature_list[,1])]
 data_test = data_test[, which(colnames(data_test) %in% feature_list[,1])]
 
-
 same_columns = intersect(colnames(data_train), colnames(data_test))
-print(same_columns)
 print("Total number of columns:")
 print(length(same_columns))
 
@@ -76,6 +78,7 @@ print(paste0('Target equals: ', target_columns))#
 
 Y_train = (data_train[, which(colnames(data_train) %in% target_columns)])
 Y_test = (data_test[, which(colnames(data_test) %in% target_columns)])
+
 
 X_train = data_train[, -which(colnames(data_train) %in% target_columns)]
 X_test = data_test[, -which(colnames(data_test) %in% target_columns)]
@@ -100,7 +103,6 @@ if(length(features_to_remove) >0) {
   X_train = X_train[,-which(colnames(X_train) %in% remove)]
   X_test = X_test[,-which(colnames(X_test) %in% remove)]
 }
-
 
 
 
@@ -133,17 +135,32 @@ X_test = X_test[, which(colnames(X_test) %in% same_columns)]
 # Train the model
 print("Training Random Forest and GLMNet prediction models")#
 d = cbind(Y_train, X_train)
+
 test_rf = randomForest(Y_train~., data=d, ntree=100, proximity=T)
 preds_rf = predict(test_rf, newdata=X_test)
 
-#varImpPlot(test_rf,  
- #         sort = T,
-  #        n.var=20,
-   #       main="Top 10 - Variable Importance")
+sample_results = sapply(1:iterations, function(x) {
+  
+  #random_features = sample( colnames(data_train), ncol(X_train))
+  X_train_r = X_train
+  X_test_r = X_test
+  
+  #X_train_r = data_train[, which(colnames(data_train) %in% random_features)]
+  #X_test_r = data_test[, which(colnames(data_test) %in% random_features)]
+  d_r = cbind(Y_train, X_train_r)
+  test_rf = randomForest(Y_train~., data=d_r, ntree=100, proximity=T)
+  preds_rf = predict(test_rf, newdata=X_test_r)
+  return(mean(100*(abs((preds_rf - Y_test))/Y_test)))
+})
 
-#var.imp = data.frame(importance(test_rf, type=2))
-#var.imp$Variables = row.names(var.imp)  
-#top = var.imp[order(var.imp$MeanDecreaseGini,decreasing = T),]
+varImpPlot(test_rf,  
+        sort = T,
+          n.var=20,
+         main="Top 10 - Variable Importance")
+
+var.imp = data.frame(importance(test_rf, type=2))
+var.imp$Variables = row.names(var.imp)  
+top = var.imp[order(var.imp$IncNodePurity,decreasing = T),]
 
 
 alphas <- seq(from = 0.0, to = 1, by = 0.1)
@@ -182,5 +199,6 @@ print("Writing file") #
 output_dir=args[7]
 dir.create(output_dir, showWarnings = TRUE, recursive = TRUE)
 write.table(results, file=paste0(output_dir, "/predictions_", number))
-
-
+rownames(top) = NULL
+write.table(top, file=paste0(output_dir, "/variable_importance_", number), row.names = FALSE)
+write.table(sample_results, file=paste0(output_dir, "/permut_results_", number), row.names = FALSE)
